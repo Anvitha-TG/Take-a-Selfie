a469fe292a5cf0fc167e941517b1e5143eaa94cd
Anvitha-TG <86748123+Anvitha-TG@users.noreply.github.com>
2022-04-04 11:40:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+SpeechRecognition = window.webkitSpeechRecognition;
+recognition = new SpeechRecognition();
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML = Content;
+}
